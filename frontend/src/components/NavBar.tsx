9a14a46b509e7cc58cd7f74a9a59676bc1972880
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-06 10:45:37 -0800

    added navbar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+//import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar expand="lg" className="navbar-dark bg-transparent">
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <Nav.Link href="#action2">About</Nav.Link>
+              <Nav.Link href="#action2">List</Nav.Link>
+            </Nav>
+            {/* <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="search-bar"
+                aria-label="Search"
+              />
+              <Button className="search-button">Search</Button>
+            </Form> */}
+            <Nav.Link className="white" href="#action1">
+              Profile
+            </Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
