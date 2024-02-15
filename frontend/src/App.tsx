f91fc3da8abf2e8bbda1d7550e5703ebb5b4b4e4
Saul999 <84159357+Saul999@users.noreply.github.com>
2024-02-15 11:52:11 -0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import React from "react";
 import NavBar from "./components/NavBar";
 import "./App.css";
-import SearchBar from "./components/SearchBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <div className="application">
+    <>
       <NavBar></NavBar>
-      <SearchBar></SearchBar>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
