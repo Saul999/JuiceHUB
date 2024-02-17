fcee9bb5fe7018b2d84e9ae93d9347a5c9288e9e
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-16 23:42:32 -0800

    song layout from bootstrap

--- frontend/src/components/ListSongs.tsx
+++ frontend/src/components/ListSongs.tsx
@@ -1,13 +1,18 @@
+import { ListGroup } from "react-bootstrap";
+
 function ListSongs() {
   return (
     <>
       <h1>Songs</h1>
 
-      <ul>
-        {" "}
-        <li>saul</li>
-        <li>ruiz</li>
-      </ul>
+      {["sm", "md", "lg", "xl", "xxl"].map((breakpoint) => (
+        <ListGroup key={breakpoint} horizontal={breakpoint} className="my-2">
+          <ListGroup.Item>This ListGroup</ListGroup.Item>
+          <ListGroup.Item>renders horizontally</ListGroup.Item>
+          <ListGroup.Item>on {breakpoint}</ListGroup.Item>
+          <ListGroup.Item>and above!</ListGroup.Item>
+        </ListGroup>
+      ))}
     </>
   );
 }
