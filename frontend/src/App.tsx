9d9a3c4ebb85593c7660617897fbfa7d8d8441af
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-17 00:53:18 -0800

    add new routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import SongListPage from "./pages/SongListPage";
+import SendReviewPage from "./pages/SendReviewPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage></HomePage>}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/list" element={<SongListPage />}></Route>
+          <Route path="/review" element={<SendReviewPage />}></Route>
+          <Route path="/profile" element={<ProfilePage />}></Route>
         </Routes>
       </Router>
     </>
