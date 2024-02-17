9c420c1ce31a36d48eccdffc3808950ad6091167
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-17 00:52:44 -0800

    add Profile, Review, SongList pages

---
+++ frontend/src/pages/SendReviewPage.tsx
@@ -0,0 +1,10 @@
+import SendReview from "../components/SendReview";
+function SendReviewPage() {
+  return (
+    <>
+      <SendReview></SendReview>
+    </>
+  );
+}
+
+export default SendReviewPage;
---
+++ frontend/src/pages/SongListPage.tsx
@@ -0,0 +1,10 @@
+import ListSongs from "../components/ListSongs";
+function SongListPage() {
+  return (
+    <>
+      <ListSongs></ListSongs>
+    </>
+  );
+}
+
+export default SongListPage;
