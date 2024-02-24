5e573282269e8aa6ab56f11bcf3473621a761f98
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-23 18:10:53 -0800

    New table type with all song data implemented

--- frontend/src/components/ListSongs.tsx
+++ frontend/src/components/ListSongs.tsx
@@ -1,20 +1,56 @@
-import { ListGroup } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface Song {
+  _id: string;
+  era: string;
+  name: string;
+  info?: string;
+  date: string;
+  type: string;
+  currentlyAvailable: "Full (CDQ)" | "Snippet (CDQ)" | "Full (HQ)";
+  circulating?: "Yes" | "No";
+}
 
 function ListSongs() {
-  return (
-    <>
-      <h1>Songs</h1>
+  const [songs, setSongs] = useState<Song[]>([]);
 
-      {["sm", "md", "lg", "xl", "xxl"].map((breakpoint) => (
-        <ListGroup key={breakpoint} horizontal={breakpoint} className="my-2">
-          <ListGroup.Item>Era</ListGroup.Item>
-          <ListGroup.Item>Name</ListGroup.Item>
-          <ListGroup.Item>Producer {breakpoint}</ListGroup.Item>
-          <ListGroup.Item>Info</ListGroup.Item>
-          <ListGroup.Item>Leaked Status</ListGroup.Item>
-        </ListGroup>
-      ))}
-    </>
+  useEffect(() => {
+    axios.get<Song[]>(`http://localhost:4000/songs`).then((response) => {
+      setSongs(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="table-container">
+      <Table striped bordered hover size="sm" responsive="lg">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Era</th>
+            <th>Title</th>
+            <th>Info</th>
+            <th>Date Leaked</th>
+            <th>Type</th>
+            <th>Circulating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {songs.map((song, index) => (
+            <tr key={song._id}>
+              <td>{index + 1}</td>
+              <td>{song.era}</td>
+              <td>{song.name}</td>
+              <td>{song.info || "-"}</td>
+              <td>{song.date}</td>
+              <td>{song.type || "-"}</td>
+              <td>{song.circulating || "-"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
