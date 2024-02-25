7bd5bb678370fdb68e688c669480ef4b1358492d
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-24 16:02:48 -0800

    added search bar function and filtering feature

--- frontend/src/components/ListSongs.tsx
+++ frontend/src/components/ListSongs.tsx
@@ -1,5 +1,7 @@
 import Table from "react-bootstrap/Table";
 import axios from "axios";
+import { Form } from "react-bootstrap";
+import { ChangeEvent } from "react";
 import { useState, useEffect } from "react";
 
 interface Song {
@@ -15,6 +17,11 @@ interface Song {
 
 function ListSongs() {
   const [songs, setSongs] = useState<Song[]>([]);
+  const [search, setSearch] = useState("");
+
+  const searchValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     axios.get<Song[]>(`http://localhost:4000/songs`).then((response) => {
@@ -23,34 +30,62 @@ function ListSongs() {
   }, []);
 
   return (
-    <div className="table-container">
-      <Table striped bordered hover size="sm" responsive="lg">
-        <thead>
-          <tr className="list-header">
-            <th>#</th>
-            <th>Era</th>
-            <th>Title</th>
-            <th>Info</th>
-            <th>Date Leaked</th>
-            <th>Type</th>
-            <th>Circulating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {songs.map((song, index) => (
-            <tr key={song._id}>
-              <td>{index + 1}</td>
-              <td>{song.era}</td>
-              <td>{song.name}</td>
-              <td>{song.info || "-"}</td>
-              <td>{song.date}</td>
-              <td>{song.type || "-"}</td>
-              <td>{song.circulating || "-"}</td>
+    <>
+      <div className="Home-search-section">
+        <img src="/assets/JWCloudBanner.jpeg" alt="juice wrld search pic" />
+        <div className="searchbar-container">
+          <Form>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Search for Song</Form.Label>
+              <Form.Control
+                onChange={searchValue}
+                type="search"
+                placeholder="Search"
+              />
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
+      <div className="table-container">
+        <Table striped bordered hover size="sm" responsive="lg">
+          <thead>
+            <tr className="list-header">
+              <th>#</th>
+              <th>Era</th>
+              <th>Title</th>
+              <th>Info</th>
+              <th>Date Leaked</th>
+              <th>Type</th>
+              <th>Circulating</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+          </thead>
+          <tbody>
+            {songs
+              .filter((item) => {
+                const lowercaseSearch = search.toLowerCase();
+                const lowercaseName = item.name.toLowerCase();
+                const lowercaseEra = item.era.toLowerCase();
+                return (
+                  lowercaseSearch === "" ||
+                  lowercaseName.includes(lowercaseSearch) ||
+                  lowercaseEra.includes(lowercaseSearch)
+                );
+              })
+              .map((song, index) => (
+                <tr key={song._id}>
+                  <td>{index + 1}</td>
+                  <td>{song.era}</td>
+                  <td>{song.name}</td>
+                  <td>{song.info || "-"}</td>
+                  <td>{song.date}</td>
+                  <td>{song.type || "-"}</td>
+                  <td>{song.circulating || "-"}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 }
 
