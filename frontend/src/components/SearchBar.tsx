27e2b34eaf54af236bf72c3918f10e4b171d5d95
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-24 16:04:31 -0800

    final searchbar

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,16 +1,28 @@
-// import { useState } from "react";
+import { useState, ChangeEvent } from "react";
+import Form from "react-bootstrap/Form";
 
 function SearchBar() {
-  // const [searched, setSearch] = useState("");
-  // const [checked, setChecked] = useState();
+  const [search, setSearch] = useState("");
+
+  const searchValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <>
       <div className="Home-search-section">
         <img src="/assets/JWCloudBanner.jpeg" alt="juice wrld search pic" />
         <div className="searchbar-container">
-          <label htmlFor="">Search </label>
-          <input type="text" />
+          <Form>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Search for Song</Form.Label>
+              <Form.Control
+                onChange={searchValue}
+                type="search"
+                placeholder="Search"
+              />
+            </Form.Group>
+          </Form>
         </div>
       </div>
     </>
