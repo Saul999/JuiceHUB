fc115c3b38e17a84afb0676575a267cbc8a38869
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-02-26 15:00:39 -0800

    fixed error displaying data of eachs SongPage

--- frontend/src/pages/SongPage.tsx
+++ frontend/src/pages/SongPage.tsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+interface Song {
+  _id: string;
+  era: string;
+  name: string;
+  info?: string;
+  date: string;
+  type: string;
+  currentlyAvailable: "Full (CDQ)" | "Snippet (CDQ)" | "Full (HQ)";
+  circulating?: "Yes" | "No";
+}
+
 function SongPage() {
+  const [song, setSongs] = useState<Song>();
+  const { SongId } = useParams();
+
+  useEffect(() => {
+    console.log("SongId:", SongId);
+    axios
+      .get<Song>(`http://localhost:4000/songs/${SongId}`)
+      .then((response) => {
+        console.log(response.data);
+
+        setSongs(response.data);
+      });
+  }, [SongId]);
   return (
-    <>
-      <h1>song page</h1>
-    </>
+    <div>
+      {song ? (
+        <>
+          <h2>{song.name}</h2>
+          <p>Era: {song.era}</p>
+          <p>Info: {song.info}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 }
 
