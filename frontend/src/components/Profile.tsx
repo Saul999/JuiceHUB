60f3820d31382601db39955d2ba67d98bd9d9ac3
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-07 17:08:59 -0800

    created log in and log out button

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,10 +1,21 @@
-import NavBar from "./NavBar";
+// import NavBar from "./NavBar";
 import Logout from "./Logout";
+import useUser from "../hooks/useUser";
+import { Link } from "react-router-dom";
+
 function Profile() {
+  const { user } = useUser();
+
   return (
     <>
       <h1>Profile page</h1>
-      <Logout></Logout>
+      {user ? (
+        <Logout></Logout>
+      ) : (
+        <Link to={"/login"}>
+          <button>Log in</button>
+        </Link>
+      )}
     </>
   );
 }
