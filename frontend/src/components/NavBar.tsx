d9904ef4f663228fe1c34f8675601833d757af64
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-07 16:58:25 -0800

    update navbar remvoing Cond rendering

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,19 +1,12 @@
-//import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-//import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import useUser from "../hooks/useUser";
-// import { Link } from "react-router-dom";
-//import NavDropdown from "react-bootstrap/NavDropdown";
+// import useUser from "../hooks/useUser";
+// import { Link } from "react-router-dom"; // Import Link from react-router-dom
+// import { NavItem } from "react-bootstrap";
 
-function NavBar() {
-  const { user, isLoading } = useUser();
-
-  // stops "login" rendering when user is logged in
-  if (user === null) {
-    return null;
-  }
+const NavBar = () => {
+  // const { user } = useUser();
 
   return (
     <>
@@ -31,29 +24,14 @@ function NavBar() {
               <Nav.Link href="/list">List</Nav.Link>
               <Nav.Link href="/review">Send Review</Nav.Link>
             </Nav>
-            {/* <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="search-bar"
-                aria-label="Search"
-              />
-              <Button className="search-button">Search</Button>
-            </Form> */}
-            {user ? (
-              <Nav.Link className="white" href="/profile">
-                Profile
-              </Nav.Link>
-            ) : (
-              <Nav.Link className="white" href="/login">
-                Log In
-              </Nav.Link>
-            )}
+            <Nav.Link className="white" href="/profile">
+              Profile
+            </Nav.Link>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
   );
-}
+};
 
 export default NavBar;
