9811be41a6efaa0f408073720713261fc2a141b9
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-03-12 19:25:06 -0700

    update profile page

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,6 +2,7 @@
 import Logout from "./Logout";
 import useUser from "../hooks/useUser";
 import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
 
 function Profile() {
   const { user } = useUser();
@@ -10,7 +11,16 @@ function Profile() {
     <>
       <h1>Profile page</h1>
       {user ? (
-        <Logout></Logout>
+        <>
+          <Image
+            src="frontend/public/assets/JWCloudBanner.jpeg"
+            roundedCircle
+          />
+
+          <h1>Name</h1>
+          <h2>Top 5 songs</h2>
+          <Logout></Logout>
+        </>
       ) : (
         <Link to={"/login"}>
           <button>Log in</button>
