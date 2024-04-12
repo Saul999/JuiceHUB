cfbf2f5284703ad91860ca6cf44f5b0cf7ded8b0
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-04-11 21:13:57 -0700

    User Model Finsihed

---
+++ backend/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = Schema({
+  UID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "moderator", "user"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
