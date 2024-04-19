bd307beeb05bfb1fce780117663883c828d7a1a6
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-04-18 23:12:48 -0700

    Add send review form skeleton

--- frontend/src/components/SendReview.tsx
+++ frontend/src/components/SendReview.tsx
@@ -1,5 +1,35 @@
+import { FormGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 function SendReview() {
-  return <h1>send review page</h1>;
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <div>
+        <h1>Send Review</h1>
+        <Form>
+          <FormGroup className="mb-3">
+            <Form.Label className="">Title of Song</Form.Label>
+            <Form.Control
+              type="title"
+              placeholder="Ex: Starstruck v2"
+              style={{ width: "400px" }}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <Form.Label>Information Review Request</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Place any information that you would like an admin to review"
+              style={{ width: "400px" }}
+            />
+          </FormGroup>
+        </Form>
+        <Button className="mb-3">Send</Button>
+      </div>
+    </div>
+  );
 }
 
 export default SendReview;
