6623b82f0dea0dee934b935cb3a596a5082ded41
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-04-26 14:43:06 -0700

    updated pages for mongo signins and logins

--- frontend/src/pages/CreateAccPage.tsx
+++ frontend/src/pages/CreateAccPage.tsx
@@ -1,57 +1,106 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+// import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Form, Button, Alert, Col, Container, Row } from "react-bootstrap";
+
 function CreateAccPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPass, setConfirmPass] = useState("");
-  const [error, setError] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confirmPass, setConfirmPass] = useState("");
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const navigate = useNavigate();
-  const createAccount = async () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     try {
-      if (password !== confirmPass) {
-        setError("Passwords do not match !");
-        return;
-      }
-      if (password.length < 6) {
-        setError("Password must be at least 6 characters long!");
-        return;
-      }
+      setLoading(true);
+      setError(false);
 
-      await createUserWithEmailAndPassword(getAuth(), email, password);
-      navigate("/profile");
+      e.preventDefault();
+      const res = await fetch("http://localhost:4000/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      setLoading(false);
     } catch {
-      setError(error);
+      setLoading(false);
+      setError(true);
     }
   };
 
+  // const navigate = useNavigate();
+  // const createAccount = async () => {
+  //   try {
+  //     if (password !== confirmPass) {
+  //       setError("Passwords do not match !");
+  //       return;
+  //     }
+  //     if (password.length < 6) {
+  //       setError("Password must be at least 6 characters long!");
+  //       return;
+  //     }
+
+  //     await createUserWithEmailAndPassword(getAuth(), email, password);
+  //     navigate("/profile");
+  //   } catch {
+  //     setError(error);
+  //   }
+  // };
+
   return (
-    <>
-      <div className="content-container">
-        <h1>Create Account Page</h1>
-        {error && <p className="error">{error}</p>}
-        <input
-          placeholder="Your email address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <input
-          type="password"
-          placeholder="Your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPass}
-          onChange={(e) => setConfirmPass(e.target.value)}
-        ></input>
-        <button onClick={createAccount}>Create Account</button>
-        <Link to="/login">Login</Link>
-      </div>
-    </>
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md="auto">
+          <h1>Create Account Page</h1>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="email"
+                placeholder="Your email address"
+                id="email"
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Control
+                type="password"
+                placeholder="Your password"
+                id="password"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" disabled={loading}>
+              {loading ? "Loading..." : "Create Account"}
+            </Button>
+            <Link to="/login" className="btn btn-link">
+              Login
+            </Link>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
@@ -18,25 +19,44 @@ function LoginPage() {
   };
 
   return (
-    <>
-      <div className="content-container">
-        <h1>Log In Page</h1>
-        {error && <p className="error">{error}</p>}
-        <input
-          placeholder="Your email address"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <input
-          type="password"
-          placeholder="Your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <button onClick={logIn}>Log in</button>
-        <Link to="/create-account">Create Account Here</Link>
-      </div>
-    </>
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md="auto">
+          <h1>Log In Page</h1>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Your email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" onClick={logIn}>
+              Log in
+            </Button>
+            <Link to="/create-account" className="ml-2">
+              Create Account Here
+            </Link>
+          </Form>
+          <div className="mt-3">
+            <Button variant="outline-danger">Sign in with Google</Button>{" "}
+            <Button variant="outline-dark">Sign in with Github</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
