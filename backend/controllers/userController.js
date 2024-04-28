87b6a5ed77bfb8c5d0985d3fdcab165b483f41d3
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-04-28 12:49:30 -0700

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,5 +5,12 @@ import { UserSchema } from "../models/userModel";
 const User = mongoose.model("User", UserSchema);
 
 export const test = (req, res) => {
-  res.json({ message: "User Test worksx" });
+  res.json({ message: "User Test works" });
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.username)
+  }
+  
+}
