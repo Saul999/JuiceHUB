b3e4333b87aa1b80024c775d3b7ade389b9c0bf0
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-04-28 13:36:02 -0700

    User needs to be signed in to send review

--- frontend/src/components/SendReview.tsx
+++ frontend/src/components/SendReview.tsx
@@ -1,34 +1,57 @@
-import { FormGroup } from "react-bootstrap";
+import { Form, FormGroup } from "react-bootstrap"; // Import FormGroup from Form
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import useUser from "../hooks/useUser";
 
 function SendReview() {
+  const { user } = useUser();
   return (
-    <div className="d-flex justify-content-center align-items-center">
-      <div>
-        <h1>Send Review</h1>
-        <Form>
-          <FormGroup className="mb-3">
-            <Form.Label className="">Title of Song</Form.Label>
-            <Form.Control
-              type="title"
-              placeholder="Ex: Starstruck v2"
-              style={{ width: "400px" }}
-            />
-          </FormGroup>
-          <FormGroup className="mb-3">
-            <Form.Label>Information Review Request</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={5}
-              placeholder="Place any information that you would like an admin to review"
-              style={{ width: "400px" }}
-            />
-          </FormGroup>
-        </Form>
-        <Button className="mb-3">Send</Button>
-      </div>
-    </div>
+    <>
+      {user ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <div>
+            <h1>Send Review</h1>
+            <Form>
+              <FormGroup className="mb-3">
+                <Form.Label className="">Title of Song</Form.Label>
+                <Form.Control
+                  type="title"
+                  placeholder="Ex: Starstruck v2"
+                  style={{ width: "400px" }}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <Form.Label>Information Review Request</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  placeholder="Place any information that you would like an admin to review"
+                  style={{ width: "400px" }}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <Form.Label>User ID</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  value={user.uid}
+                  readOnly
+                  style={{ width: "400px" }}
+                />
+              </FormGroup>
+
+              <Button className="mb-3">Send</Button>
+            </Form>
+          </div>
+        </div>
+      ) : (
+        <>
+          <h1>If you want to contribute to a song's information, sign in</h1>
+          <Link to="/login">
+            <Button>Log in</Button>{" "}
+          </Link>
+        </>
+      )}
+    </>
   );
 }
 
