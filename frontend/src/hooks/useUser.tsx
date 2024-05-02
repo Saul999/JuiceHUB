59aada74fb328153467abcc03603ca18996dcf43
Saul385 <84159357+Saul999@users.noreply.github.com>
2024-05-02 11:06:50 -0700

    fixed build errors with firebase

--- frontend/src/hooks/useUser.tsx
+++ frontend/src/hooks/useUser.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth"; // Import User type
+import { app } from "../config/firebase";
 
 const useUser = () => {
   const [user, setUser] = useState<User | null>(null); // Define user as User | null
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const auth = getAuth(); // Get authentication instance
+    const auth = getAuth(app); // Get authentication instance
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       // Use onAuthStateChanged
       setUser(user);
